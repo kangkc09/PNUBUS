f30b99d556bf39e3a6c0cb126181f858c55fc2c7
Yerin99 <diluny2@gmail.com>
2021-05-29 17:05:11 +0900

    1704

--- app/src/main/java/com/example/termproject/MainActivity.java
+++ app/src/main/java/com/example/termproject/MainActivity.java
@@ -1,15 +1,54 @@
 package com.example.termproject;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState){
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
 
+        Button imageButton = (Button) findViewById(R.id.button);
+        imageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), OntimeBus.class);
+                startActivity(intent);
+            }
+        });
+
+        Button imageButton2 = (Button) findViewById(R.id.button2);
+        imageButton2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), BusStop.class);
+                startActivity(intent);
+            }
+        });
+
+        Button imageButton3 = (Button) findViewById(R.id.button3);
+        imageButton3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), BusInformation.class);
+                startActivity(intent);
+            }
+        });
+
+        Button imageButton4 = (Button) findViewById(R.id.button4);
+        imageButton4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), Notice.class);
+                startActivity(intent);
+            }
+        });
+    }
 }
