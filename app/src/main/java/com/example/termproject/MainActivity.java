e85cf799b3e0494df6944ccb3ca4def0a6d39483
kangkc09 <62276515+kangkc09@users.noreply.github.com>
2021-05-29 17:04:03 +0900

    17:03

--- app/src/main/java/com/example/termproject/MainActivity.java
+++ app/src/main/java/com/example/termproject/MainActivity.java
@@ -1,15 +1,29 @@
 package com.example.termproject;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 
 public class MainActivity extends AppCompatActivity {
+    private Button btn4;
 
     @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btn4=(Button) findViewById(R.id.button4);
+        btn4.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Intent Intent =new Intent(MainActivity.this,Notice.class);
+                startActivity(Intent);
+            }
+        });
     }
 
 }
--- app/src/main/java/com/example/termproject/Notice.java
+++ app/src/main/java/com/example/termproject/Notice.java
@@ -2,13 +2,25 @@ package com.example.termproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Notice extends AppCompatActivity {
-
+    private Button btn5;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_notice);
+
+        btn5=(Button) findViewById(R.id.button5);
+        btn5.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Intent Intent =new Intent(Notice.this,Notice1.class);
+                startActivity(Intent);
+            }
+        });
     }
 }
