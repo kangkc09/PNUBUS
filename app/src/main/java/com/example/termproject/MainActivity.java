2a9584d83b751692e217a15ede7979481369e186
kangkc09 <62276515+kangkc09@users.noreply.github.com>
2021-06-19 11:38:46 +0900

    navermap

--- app/src/main/java/com/example/termproject/MainActivity.java
+++ app/src/main/java/com/example/termproject/MainActivity.java
@@ -6,10 +6,12 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 
 public class MainActivity extends AppCompatActivity {
 
+    private long backBtnTime=0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -50,5 +52,18 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+    }
+    @Override
+    public void onBackPressed(){
+        long curTime=System.currentTimeMillis();
+        long gapTime=curTime-backBtnTime;
+
+        if(0<=gapTime&& 2000>=gapTime){
+            super.onBackPressed();
+        }
+        else{
+            backBtnTime=curTime;
+            Toast.makeText(this,"한번 더 누르면 종료됩니다.",Toast.LENGTH_SHORT).show();
+        }
     }
 }
