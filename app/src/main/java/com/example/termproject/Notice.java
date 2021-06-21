8c6e23c27536bf6c372e04b514ed0b6b54fb8b72
Yerin99 <diluny2@gmail.com>
2021-06-21 16:10:39 +0900

    1610

--- app/src/main/java/com/example/termproject/Notice.java
+++ app/src/main/java/com/example/termproject/Notice.java
@@ -9,17 +9,28 @@ import android.view.View;
 import android.widget.Button;
 
 public class Notice extends AppCompatActivity {
-    private Button btn5;
+    private Button notice1;
+    private Button notice2;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_notice);
 
-        btn5=(Button) findViewById(R.id.button5);
-        btn5.setOnClickListener(new View.OnClickListener(){
+        notice1 = (Button) findViewById(R.id.notice1);
+        notice1.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v){
                 Intent Intent =new Intent(Notice.this,Notice1.class);
+                startActivity(Intent);
+            }
+        });
+
+        notice2 = (Button) findViewById(R.id.notice2);
+        notice2.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Intent Intent =new Intent(Notice.this,Notice2.class);
                 startActivity(Intent);
             }
         });
